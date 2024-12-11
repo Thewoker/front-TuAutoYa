89c7862f5f1e767550ada2df0b34915eaeebc400
Laura jimenez <emmabluebeachwaer@gmail.com>
2024-12-11 11:16:15 -0500

    Hello everyone this is commit #1 of the Lau branch

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import Header from '@/components/Header'
-import Main from '@/components/Home'
+
+
 
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <Main />
+      
     </div>
   );
 }
