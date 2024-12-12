861b0989a8f92557ce8b8cfabab162562b5b9266
Laura <emmabluebeachwear@gmail.com>
2024-12-12 10:12:35 -0500

    saving changes before switching branches

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import Header from '@/components/Header'
-import Main from '@/components/Home'
+
 
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <Main />
+      
     </div>
   );
 }
