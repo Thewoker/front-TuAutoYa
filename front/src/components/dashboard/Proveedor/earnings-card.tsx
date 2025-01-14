adf84d06ccec2dd67b6b6d43fe13fa9fb71f8442
Leonel FJR <83437197+Thewoker@users.noreply.github.com>
2025-01-14 19:07:43 -0300

    deploy

--- front/src/components/dashboard/Proveedor/earnings-card.tsx
+++ front/src/components/dashboard/Proveedor/earnings-card.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { UserEarnings } from '@/Interfaces/earnings'
-import { getDate } from 'date-fns'
 
 interface EarningsCardProps {
   user: UserEarnings['user'];
